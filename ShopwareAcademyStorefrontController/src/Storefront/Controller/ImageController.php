025a23b287e29469956897bca613f13cea5f43f2
Shem <smagnezi8@gmail.com>
2025-01-05 14:41:21 +0000

    feat: enhance image display functionality by adding product ID to image route and updating service dependencies

--- ShopwareAcademyStorefrontController/src/Storefront/Controller/ImageController.php
+++ ShopwareAcademyStorefrontController/src/Storefront/Controller/ImageController.php
@@ -8,23 +8,28 @@ use Symfony\Component\HttpFoundation\Response;
 use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Symfony\Component\HttpFoundation\Request;
 
 #[Route(defaults: ['_routeScope' => ['storefront'], 'XmlHttpRequest' => true])]
 class ImageController extends StorefrontController
 {
     public function __construct(
         private readonly HttpClientInterface $client,
-        private readonly SystemConfigService $systemConfigService
+        private readonly SystemConfigService $systemConfigService,
+        private readonly EntityRepository $productRepository
     )
     {
     }
 
     #[Route(
-        path: '/image',
+        path: '/image/{productId}',
         name: 'frontend.image.show',
+        defaults: ['productId' => null],
         methods: ['GET']
     )]
-    public function showImage(SalesChannelContext $context): Response
+    public function showImage(?string $productId = null, SalesChannelContext $context): Response
     {
         $apiAccessKey = $this->systemConfigService->get('AcademyStorefrontController.config.apiAccessKey', $context->getSalesChannelId());
         $apiProvider = $this->systemConfigService->get('AcademyStorefrontController.config.apiProvider', $context->getSalesChannelId());
